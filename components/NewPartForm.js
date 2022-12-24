6a51a8b0526ca344ee28b777056d00d33ad467fc
jprudolph03 <63522574+jprudolph03@users.noreply.github.com>
2022-12-23 20:10:40 -0500

    added to newpart form

--- components/NewPartForm.js
+++ components/NewPartForm.js
@@ -4,6 +4,7 @@ import { Form, Button } from "semantic-ui-react";
 
 const NewPartForm = () => {
   const [formSubSuccess, setFormSubSuccess] = useState(null);
+  const [machType, setMachType] = useState(null);
   const router = Router;
 
   const handleNewPartSubmit = async (e) => {
@@ -13,10 +14,13 @@ const NewPartForm = () => {
       name: e.target[0].value,
       lotPrefix: e.target[1].value,
       STDLoss: e.target[2].value,
+      DPTLoss: e.target[3].value,
+      material: e.target[4].value,
+      machType: e.target[5].value,
     };
 
     const JSONdata = JSON.stringify(data);
-    console.log(JSONdata);
+    // console.log(JSONdata);
 
     const endpoint = "https://scraptrackbe.onrender.com/api/part";
 
@@ -35,6 +39,8 @@ const NewPartForm = () => {
     router.push("/");
   };
 
+  const handleMachChange = (e, { value }) => setMachType({ value });
+
   return (
     <Form onSubmit={handleNewPartSubmit}>
       <Form.Field>
@@ -49,6 +55,29 @@ const NewPartForm = () => {
         <label>Coil Loss</label>
         <input placeholder="coil loss" type="number" step={"0.01"} />
       </Form.Field>
+      <Form.Field>
+        <label>DPT Loss:</label>
+        <input placeholder="DPT Loss" type="number" step={"0.01"} />
+      </Form.Field>
+      <Form.Field>
+        <label>Material</label>
+        <input placeholder="Material" type="text" step={"0.01"} />
+      </Form.Field>
+      <Form.Group inline>
+        <label>Machine:</label>
+        <Form.Radio
+          label="AOL"
+          value="aol"
+          // checked={value === "aol"}
+          onChange={handleMachChange}
+        />
+        <Form.Radio
+          label="RF"
+          value="rf"
+          // checked={value === "rf"}
+          onChange={handleMachChange}
+        />
+      </Form.Group>
       <Button type="submit" color="green">
         Submit
       </Button>
